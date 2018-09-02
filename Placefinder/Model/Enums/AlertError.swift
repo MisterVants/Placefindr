36192ba49facf2eae7ae48e614992cb55621d046
MrVants <andrevants@gmail.com>
2018-09-02 11:47:58 -0300

    App working for basic features and reasonable UX

---
+++ Placefinder/Model/Enums/AlertError.swift
@@ -0,0 +1,58 @@
+//
+//  AlertError.swift
+//  Placefinder
+//
+//  Created by André Vants Soares de Almeida on 01/09/18.
+//
+
+import Foundation
+
+protocol AlertError {
+    var alertTitle: String {get}
+    var alertMessage: String {get}
+}
+
+enum PlacefinderError: AlertError {
+    // Location related
+    case locationServicesDisabled
+    case locationDenied
+    case unknownLocation
+    // Search & Request related
+    case nearbyPlaceRequestFailed
+    case placeByIdRequestFailed
+    case noResultsReturned
+    
+    var alertTitle: String {
+        switch self {
+        case .locationServicesDisabled:
+            return NSLocalizedString("Location Services Disabled", comment: "")
+        case .locationDenied:
+            return NSLocalizedString("Location Denied", comment: "")
+        case .unknownLocation:
+            return NSLocalizedString("Unknown Location", comment: "")
+        case .nearbyPlaceRequestFailed:
+            return NSLocalizedString("Nearby Request Failed", comment: "")
+        case .placeByIdRequestFailed:
+            return NSLocalizedString("Place Id Request Failed", comment: "")
+        case .noResultsReturned:
+            return NSLocalizedString("No Place Results", comment: "")
+        }
+    }
+    
+    var alertMessage: String {
+        switch self {
+        case .locationServicesDisabled:
+            return NSLocalizedString("Location Disabled Description", comment: "")
+        case .locationDenied:
+            return NSLocalizedString("Location Denied Description", comment: "")
+        case .unknownLocation:
+            return NSLocalizedString("Unknown Location Description", comment: "")
+        case .nearbyPlaceRequestFailed:
+            return NSLocalizedString("Nearby Request Failed Description", comment: "")
+        case .placeByIdRequestFailed:
+            return NSLocalizedString("Place Id Request Failed Description", comment: "")
+        case .noResultsReturned:
+            return NSLocalizedString("No Place Results Description", comment: "")
+        }
+    }
+}
---
+++ Placefinder/Model/Enums/LocationServiceStatus.swift
@@ -0,0 +1,13 @@
+//
+//  LocationServiceStatus.swift
+//  Placefinder
+//
+//  Created by André Vants Soares de Almeida on 01/09/18.
+//
+
+enum LocationServiceStatus {
+    case serviceDisabled
+    case notAuthorized
+    case ok
+}
+
